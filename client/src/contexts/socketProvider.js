abc9062d6430eba43bac6340d4f6a62efe5d1c02
avarlamova <59831804+avarlamova@users.noreply.github.com>
2020-12-21 23:12:15 +0300

    fixed wrong dependencies
    socket and socket.io used to have different versions

--- client/src/contexts/socketProvider.js
+++ client/src/contexts/socketProvider.js
@@ -1,24 +1,28 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react'
 import io from 'socket.io-client'
 
-const SocketContext = React.createContext();
+const SocketContext = React.createContext()
 
 export function useSocket() {
-    return useContext(SocketContext)
+  return useContext(SocketContext)
 }
 
-export function SocketProvider({ login, children}) {
-    const [socket, setSocket] = useState();
+export function SocketProvider({ login, children }) {
+  const [socket, setSocket] = useState()
 
-    useEffect(() => {
-        const newSocket = io('http://localhost:5000', { query: { login } })
-        setSocket(newSocket)
-        return () => newSocket.close()
-    }, [login])
-
-    return (
-        <SocketContext.Provider value={socket}>
-            {children}
-        </SocketContext.Provider>
+  useEffect(() => {
+    const newSocket = io(
+      'http://localhost:5000',
+      { query: { login } }
     )
+    setSocket(newSocket)
+
+    return () => newSocket.close()
+  }, [login])
+
+  return (
+    <SocketContext.Provider value={socket}>
+      {children}
+    </SocketContext.Provider>
+  )
 }
--- server/server.js
+++ server/server.js
@@ -1,19 +1,16 @@
-//const io = require('socket.io')(5000, { cors: { origin: '*' }})
-const io = require ('socket.io') (5000)
+const io = require('socket.io')(5000)
 
 io.on('connection', socket => {
-    const login = socket.handshake.query.login;
-    socket.join(login)
+  const login = socket.handshake.query.login
+  socket.join(login)
 
-    socket.on('send-message', ({receivers, message}) => {
-        receivers.forEach(receiver => {
-            const newReceivers = receivers.filter(r => r!==receiver)
-            newReceivers.push(login)
-            socket.broadcast.to(receiver).emit('receive-message', {
-                receivers: newReceivers,
-                sender: login, 
-                message
-            })
-        });
+  socket.on('send-message', ({ recipients, message }) => {
+    recipients.forEach(recipient => {
+      const newRecipients = recipients.filter(r => r !== recipient)
+      newRecipients.push(login)
+      socket.broadcast.to(recipient).emit('receive-message', {
+        recipients: newRecipients, sender: login, message
+      })
     })
+  })
 })
